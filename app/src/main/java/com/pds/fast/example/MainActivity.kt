b612b5ea345a5d6ca83c161a6d7e4656384bd480
pengdaosong <pengdaosong@medlinker.com>
2021-08-19 17:14:13 +0800

    Initial commit

---
+++ app/src/main/java/com/pds/fast/example/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pds.fast.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pds.fast.ui.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
