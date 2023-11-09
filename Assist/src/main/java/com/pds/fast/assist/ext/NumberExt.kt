c3c7eb2d8b62c24ff0869f709ebe416690b2ec8f
CERENCE\daosong.peng <daosong.peng@cerence.com>
2023-11-09 11:11:17 +0800

    doc

--- Assist/src/main/java/com/pds/fast/assist/ext/NumberExt.kt
+++ Assist/src/main/java/com/pds/fast/assist/ext/NumberExt.kt
@@ -2,6 +2,9 @@ package com.pds.fast.assist.ext
 
 import android.content.res.Resources
 import android.util.TypedValue
+import com.pds.fast.assist.Assist
 
 val Float.dp
     get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, Resources.getSystem().displayMetrics)
+
+fun Int.str(vararg args: Any?) = Assist.application.resources.getString(this, *args)
--- app/src/main/java/com/pds/fast/example/App.kt
+++ app/src/main/java/com/pds/fast/example/App.kt
@@ -2,9 +2,12 @@ package com.pds.fast.example
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
 import androidx.multidex.MultiDex
 import androidx.multidex.MultiDexApplication
 import com.pds.fast.assist.Assist
+import com.pds.fast.assist.ext.str
+import com.pds.fast.ui.R
 
 class App : MultiDexApplication() {
     companion object {
@@ -20,5 +23,6 @@ class App : MultiDexApplication() {
         super.onCreate()
         app = this
         Assist.init(this)
+        Log.e("113333", "333=${R.string.wake_up_tip111.str(null)}")
     }
 }
